294946ff184e7cfbfba4a897c724d96877b4d7a8
soham8167 <142341933+soham8167@users.noreply.github.com>
2024-10-15 18:26:03 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ import { User } from './schema/user.js';
 const app = express();
 
 app.use(cors({
-    origin: 'https://meetup-zeta-vert.vercel.app',
+    origin: 'https://video-call-mern-frontend.vercel.app/',
     methods: ['GET', 'POST', 'DELETE'],
     // allowedHeaders: ['Content-Type', 'Authorization'],
     credentials: true
